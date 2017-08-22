c1320de5018af77700490a90bf4523e0dbaa3aa0
Jared Eiseman <jathei@gmail.com>
2017-08-21 18:12:45 -0700

    react boilerplate

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(props){
+  return (
+    <div>
+      <h1>Stuff to render goes here!</h1>
+      <p>Remember to change the title of the page in webpack.config.js, as well as the project name in package.json etc. (and restart the git repo).</p>
+    </div>
+  );
+}
+
+export default App;
